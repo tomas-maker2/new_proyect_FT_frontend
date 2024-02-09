08553f9d290f7cffba4cf6c856fe04b36da23394
tomas-maker2 <tbolotnicoff@gmail.com>
2024-02-09 14:28:05 -0300

    frontend2

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -13,7 +13,7 @@ const ProductCard = (props) => {
   
   return (
     <>
-      <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
+      <div className={`${location.pathname === "/product" ? `gr-${grid}` : "col-3"}`}>
         <Link to={'/product/:id'} className="product-card position-relative">
             <div className="wishlist-icon position-absolute"> 
                 <button className="border-0 bg-transparent">
